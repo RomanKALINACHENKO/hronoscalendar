96e7dc076cfe4d5485390544de4abfa26c46a9ff
Roman Kalinachenko <rkalinachenko@gmail.com>
2020-05-25 00:57:59 +0300

    Updates

---
+++ precache-manifest.6a767d394e51642cd50a77affae44c3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65cdaf2304cc8ca2b80e",
+    "url": "/hronoscalendar/static/css/main.20387da2.chunk.css"
+  },
+  {
+    "revision": "65cdaf2304cc8ca2b80e",
+    "url": "/hronoscalendar/static/js/main.4429c8fa.chunk.js"
+  },
+  {
+    "revision": "8ad428ec4a03738bd4e2",
+    "url": "/hronoscalendar/static/js/runtime~main.0fe9d0bd.js"
+  },
+  {
+    "revision": "6da849539d25590f0e99",
+    "url": "/hronoscalendar/static/js/2.bc219374.chunk.js"
+  },
+  {
+    "revision": "f9dba29756d233641cf13879069fe031",
+    "url": "/hronoscalendar/index.html"
+  }
+];
